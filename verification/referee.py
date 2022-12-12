d33440cd81f148b91fcc5a3876ff9a87f803620f
CDG.Axel <92265470+CDG-Axel@users.noreply.github.com>
2022-12-12 15:21:53 +0300

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,8 +38,8 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "extremely_ugly",
-            "js": "extremely_ugly"
+            "python": "huffman_encode",
+            "js": "huffman_encode"
         },
         cover_code={
             'python-3': cover_codes.unwrap_args,
